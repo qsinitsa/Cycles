75a56f35801be18b2575675756a3c15985f063b5
qsinitsa <qsinitsa@gmail.com>
2022-10-06 20:06:54 +0300

    Work is done

--- src/Task8.java
+++ src/Task8.java
@@ -1,11 +1,11 @@
 public class Task8 {
     public static void main(String[] args) {
         int salary = 29000;
-        int total = 0;
-        int percent;
+        double total = 0;
+        double percent;
         for (int i = 1; i<= 12; i++){
             percent = total / 100;
-            total += salary;
+            total += salary + percent;
             System.out.println("Месяц " + i + " сумма накоплений равна " + total );
         }
     }
